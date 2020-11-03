e2dd1d47640f6db725a02572b21e5e9a302af051
mmelucci <mmelucci20@stuy.edu>
2020-11-03 00:11:44 -0500

    Completed the SuperArray method and size method, along with a test

---
+++ SuperArray.java
@@ -0,0 +1,13 @@
+public class SuperArray {
+  private String [] data;
+  private int size;
+
+  public SuperArray() {
+    this.data = new String[10];
+    this.size = 0;
+  }
+
+  public int size() {
+    return this.size;
+  }
+}
---
+++ SuperArrayTester.java
@@ -0,0 +1,7 @@
+public class SuperArrayTester {
+  public static void main(String[] args) {
+    SuperArray words = new SuperArray();
+    System.out.println("Check Size:");
+    System.out.println(words.size());
+  }
+}
