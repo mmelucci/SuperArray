e4c7c16f2e1bdc19119f9ec7f098d77d1debd121
mmelucci <mmelucci20@stuy.edu>
2020-11-05 12:42:24 -0500

    Fixed remove method

--- SuperArrayTester.java
+++ SuperArrayTester.java
@@ -40,7 +40,7 @@ public class SuperArrayTester {
     words.remove(4);
     System.out.println(words.toString());
     System.out.println(words.size());
-    words.remove(0);
+    words.remove(15);
     System.out.println(words.toString());
     System.out.println(words.size());
     words.add(4, "Works");
