a3901849bea6cfcf478778123dcaaf6de7336ba9
mmelucci <mmelucci20@stuy.edu>
2020-11-09 00:42:29 -0500

    Added findOverlap method and test

--- Demo.java
+++ Demo.java
@@ -7,6 +7,17 @@ public class Demo {
     }
   }
 
+  public static SuperArray findOverlap(SuperArray a, SuperArray b) {
+    SuperArray temp = new SuperArray ();
+    for (int i = 0; i < a.size(); i ++) {
+      if (b.contains(a.get(i))) {
+        temp.add(a.get(i));
+      }
+    }
+    removeDuplicates(temp);
+    return temp;
+  }
+
   public static void main(String[]args){
     SuperArray words = new SuperArray();
     //grouped to save vertical space
@@ -16,5 +27,14 @@ public class Demo {
 
     System.out.println(words);
     removeDuplicates(words);
-    System.out.println(words);  }
+    System.out.println(words);
+
+    SuperArray a = new SuperArray();
+    SuperArray b = new SuperArray();
+    a.add("9");   a.add("1");     a.add("2");
+    a.add("2");   a.add("3");     a.add("4");
+    b.add("0");   b.add("4");     b.add("2");
+    b.add("2");   b.add("9");
+    System.out.println(findOverlap(a, b));
+  }
 }
